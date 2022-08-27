f27524deae07bbd614e376eb820e6f309ba5065f
Jacob Lewinski <jdlewinski@outlook.com>
2022-08-27 15:59:00 -0500

    Remove unused code

--- Wizard/Extensions/Game.cs
+++ Wizard/Extensions/Game.cs
@@ -7,22 +7,9 @@ namespace Wizard.Extensions
 {
     public static class GameExtension
     {
-        private class CurentRound
-        {
-            private IEnumerable<IRoundResult> _roundResult;
-
-            public CurentRound(IEnumerable<IRoundResult> roundResults)
-            {
-                _roundResult = roundResults;
-            }
-
-            public int TotalResult => _roundResult.Sum(x => x.Result);
-        }
-
         public static (bool success, int roundNumber, IReadOnlyList<string> errors) NextRound(this IGame game, int roundNumber, bool save, IDataService dataService = null)
         {
             var errors = new List<string>();
-            var CurrentRound = new CurentRound(game.Players.Select(x => x.Rounds.Last()));
             var totalResult = game.Players.Sum(x => x.Rounds.ElementAt(roundNumber - 1).Result);
             if (roundNumber != 60 / game.Players.Count)
             {

f5c071556fbd0c1d4a0f5716749d798141adf15e
Jacob Lewinski <jdlewinski@outlook.com>
2022-08-27 22:40:02 -0500

    Same error different file

--- Wizard.MauiMobile/ViewModels/MainViewModel.cs
+++ Wizard.MauiMobile/ViewModels/MainViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Wizard.Models;
 using Wizard.Services;
+using Wizard.Services.Settings;
 
 namespace Wizard.MauiMobile.ViewModels
 {
@@ -13,7 +14,8 @@ namespace Wizard.MauiMobile.ViewModels
         private readonly IDataService _dataService;
         private List<GameSummary> _gameList;
 
-        public MainViewModel(IDataService dataService)
+        public MainViewModel(IDataService dataService, ISettingsService settings)
+            : base(settings)
         {
             _dataService = dataService;
             _gameList = dataService.GetSummaries();
